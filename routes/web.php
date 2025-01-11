4f90a73a7b8a3b651f5555a7fa121cfd58a778bc
brianson dzvairo <briansont.dzvairo@gmail.com>
2025-01-11 17:43:52 +0200

    Update return about view

--- routes/web.php
+++ routes/web.php
@@ -28,7 +28,7 @@ Route::get('user/{user:name}', [\App\Http\Controllers\UserController::class, 'sh
 // resources/views/pages/about.blade.php - without any controller
 // Also, assign the route name "about"
 // Put one code line here below
-Route::get('/about', 'about')->name('about');
+Route::view('/about', 'about')->name('about');
 
 
 // Task 4: redirect the GET URL "log-in" to a URL "login"
